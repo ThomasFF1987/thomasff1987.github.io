945da1fba56e57b3fcb1e4b2a7e0c3bd863318db
Thomas Baggi-Sani <thomas.baggi@live.fr>
2025-12-01 10:17:43 +0100

    Correction vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/thomasff1987.github.io/',
+  base: '/react-portfolio/',
   plugins: [react()],
 })
